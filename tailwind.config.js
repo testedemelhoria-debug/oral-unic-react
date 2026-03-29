005994594b4ea47bceefd07d017d9a1d717b2a46
testedemelhoria-debug <testedemelhoria@gmail.com>
2026-03-29 15:47:04 -0300

    Format content array in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
         turquesa: '#5BC8C8',
         escuro: '#0F2A1E',
       },
+      fontFamily: {
+        serif: ['Playfair Display', 'serif'],
+        sans: ['Inter', 'sans-serif'],
+      },
     },
   },
   plugins: [],
 }
-
